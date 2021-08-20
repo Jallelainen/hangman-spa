3038075dbd8285fe006c6f678e3a05292d7c5863
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2021-08-20 11:04:19 +0200

    Fixed api call to return only word object

--- src/API/GameService.js
+++ src/API/GameService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 class GameService {
-    async getWord(){
+    async getWord(setWordObj){
         return await axios
             .get("https://random-words-api.vercel.app/word")
             .then((response) => {
-                console.log(response);
-                return response.data;
+                console.log(response.data);
+                setWordObj(response.data[0]);
             })
             .catch((error) => {console.log(error)});
     }
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -3,13 +3,20 @@ import { useState, useEffect } from 'react';
 import GameService from '../API/GameService';
 
 export const Game = (props) => {
-    const [wordObj, setWordObj] = useState(GameService.getWord());
+    const [wordObj, setWordObj] = useState();
 
-    console.log(wordObj);
+    useEffect(() => {
+        GameService.getWord(setWordObj);
+    }, []);
+
+console.log(wordObj);
 
     return (
         <div className="main-content">
             <h2>Guess the Word</h2>
+            {
+                wordObj? <p>{wordObj.word}</p> : <h4>"Loading..."</h4>
+            }
             <button className="neg-button" onClick={() => {props.setGameState("new")}}>Cancel Game</button>
         </div>
     )
