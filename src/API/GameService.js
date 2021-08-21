b7b29be8897b8370cfef633b333c04c47abd9cbb
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2021-08-21 09:16:53 +0200

    Added some comments

--- src/API/GameService.js
+++ src/API/GameService.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 class GameService {
+
+    /* API get request that fetches the word, then assigns word, hint and hidden word to hooks */
     async getWord(setWord, setHiddenWord, setHint){
         return await axios
             .get("https://random-words-api.vercel.app/word")
             .then((response) => {
                 console.log(response.data);
+
                 setHiddenWord("_".repeat(response.data[0].word.length));
                 setWord(response.data[0].word);
                 setHint(response.data[0].definition);
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -10,6 +10,7 @@ export const Game = (props) => {
     const [guessedChars, setGuessedChars] = useState([]);
     const [round, setRound] = useState(0);
 
+    /* Fetch word */
     useEffect(() => {
         GameService.getWord(setWord, setHiddenWord, setHint);
     }, []);
@@ -18,29 +19,33 @@ export const Game = (props) => {
         let updated = hiddenWord;
         let rounds = round;
         
+        /* Checks if maximum amount of tries is achieved*/
         if (rounds < 9) {
+            /* Goes through each letter of the word and checks it against the guess */
             for (let i = 0; i < word?.length; i++) {
                 if (word[i].toUpperCase() === guess.toUpperCase()) {
-                    updated = setCharAt(updated, i, guess);
+                    updated = setCharAt(updated, i, guess); //If it matches, a defined function inserts the guessed character into the right place in the hidden word
                 }     
             };
             
+            /* This checks if the guess was added to the hidden word. It also checks if the guess already has been made, if not, the guess will be saved in an array */
             if (updated.toUpperCase() === hiddenWord.toUpperCase()) {
                 
                 if(!guessedChars.includes(guess)){
                     setGuessedChars(guessedChars.concat(guess));
                     rounds++;
                 }
-            }else if (updated.toUpperCase() === word.toUpperCase()) {
+            }else if (updated.toUpperCase() === word.toUpperCase()) { /* If the guess wasn't added to the hidden word, it checks if the hidden word is  */
                 props.setGameState("won")
             }
             
             
         }else{
-            rounds++;
+            /* If the round exceeds ten iterations, gamestate will be set to 'lost' */
             props.setGameState("lost");
         }
 
+        /* Hooks are updated last since they take more time and cant keep up with loop-iterations, input field is also cleared */
         setHiddenWord(updated.toUpperCase());
         setRound(rounds);
 
@@ -51,8 +56,7 @@ export const Game = (props) => {
         <div className="main-content">
             <h2>Guess the Word</h2>
             {hiddenWord? <h4>{hiddenWord}</h4> : <h3>Loading...</h3>}
-            <p><b>Failed guesses:</b> {round}</p>
-            <p><b>Used letters:</b> {guessedChars}</p> 
+            <p><b>Failed guesses:</b> {round} <b>Used letters:</b> {guessedChars}</p>
             <input id="guess-input" placeholder="Enter you guess here..." type="text" onChange={(e) => handleInput(e.target.value)}/>
             <button className="neg-button" onClick={() => {props.setGameState("new")}}>Cancel Game</button>
         </div>
