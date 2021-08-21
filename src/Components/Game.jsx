e3c1cb400560cac160a868e74bd43e84cac9fba5
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2021-08-21 11:55:00 +0200

    fixed logic to update images when guess fail

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -19,23 +19,24 @@ export const Game = (props) => {
         let updated = hiddenWord;
         let rounds = round;
         
-        /* Checks if maximum amount of tries is achieved*/
-        if (rounds < 9) {
-            /* Goes through each letter of the word and checks it against the guess */
-            for (let i = 0; i < word?.length; i++) {
-                if (word[i].toUpperCase() === guess.toUpperCase()) {
-                    updated = setCharAt(updated, i, guess); //If it matches, a defined function inserts the guessed character into the right place in the hidden word
-                }     
-            };
-            
-            /* This checks if the guess was added to the hidden word. It also checks if the guess already has been made, if not, the guess will be saved in an array */
-            if (updated.toUpperCase() === hiddenWord.toUpperCase()) {
+        if (word) {
+            /* Checks if maximum amount of tries is achieved*/
+            if (rounds < 9) {
+                /* Goes through each letter of the word and checks it against the guess */
+                for (let i = 0; i < word?.length; i++) {
+                    if (word[i].toUpperCase() === guess.toUpperCase()) {
+                        updated = setCharAt(updated, i, guess); //If it matches, a defined function inserts the guessed character into the right place in the hidden word
+                    }     
+                };
                 
-                if(!guessedChars.includes(guess) && !word.toUpperCase().includes(guess.toUpperCase())){
-                    setGuessedChars(guessedChars.concat(guess));
-                    rounds++;
-                }
-            }else if (updated.toUpperCase() === word.toUpperCase()) { /* If the guess wasn't added to the hidden word, it checks if the hidden word is  */
+                /* This checks if the guess was added to the hidden word. It also checks if the guess already has been made, if not, the guess will be saved in an array */
+                if (updated.toUpperCase() === hiddenWord.toUpperCase()) {
+                    
+                    if(!guessedChars.includes(guess) && !word.toUpperCase().includes(guess.toUpperCase())){
+                        setGuessedChars(guessedChars.concat(guess));
+                        rounds++;
+                    }
+                }else if (updated.toUpperCase() === word.toUpperCase()) { /* If the guess wasn't added to the hidden word, it checks if the hidden word is  */
                 props.setGameState("won")
             }
             
@@ -44,17 +45,21 @@ export const Game = (props) => {
             /* If the round exceeds ten iterations, gamestate will be set to 'lost' */
             props.setGameState("lost");
         }
-
+        
         /* Hooks are updated last since they take more time and cant keep up with loop-iterations, input field is also cleared */
         setHiddenWord(updated.toUpperCase());
         setRound(rounds);
-
+        
         document.getElementById("guess-input").value = '';
+    }
     };
 
     return (
         <div className="main-content">
             <h2>Guess the Word</h2>
+            <div className="img-container">
+                {round > 0 ? <img className="game-pic" src={"../Pics/hangman" + round +".png"} alt="Hangmans noose picture"/> : <div className="game-pic"></div>}
+            </div>
             {hiddenWord? <h4>{hiddenWord}</h4> : <h3>Loading...</h3>}
             <p><b>Used letters:</b> {guessedChars}</p>
             {round > 0 ? <p><b>Failed guesses:</b> {round} </p> : <div></div>}
